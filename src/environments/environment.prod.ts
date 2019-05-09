7f91986a10087a8511899b979d39c8c6a467ec98
pranavsriram8 <pranavsriram8@gmail.com>
2019-05-09 14:12:56 +0530

    added the components

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCJrvO4sUshn2SlHWC-yXohX4ipFEadsZc",
+    authDomain: "chatapplication-b47db.firebaseapp.com",
+    databaseURL: "https://chatapplication-b47db.firebaseio.com",
+    projectId: "chatapplication-b47db",
+    storageBucket: "chatapplication-b47db.appspot.com",
+    messagingSenderId: "182895035852",
+    appId: "1:182895035852:web:47e4ae46ca75c5ad"
+    
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCJrvO4sUshn2SlHWC-yXohX4ipFEadsZc",
+    authDomain: "chatapplication-b47db.firebaseapp.com",
+    databaseURL: "https://chatapplication-b47db.firebaseio.com",
+    projectId: "chatapplication-b47db",
+    storageBucket: "chatapplication-b47db.appspot.com",
+    messagingSenderId: "182895035852",
+    appId: "1:182895035852:web:47e4ae46ca75c5ad"
+  }
 };
 
 /*
